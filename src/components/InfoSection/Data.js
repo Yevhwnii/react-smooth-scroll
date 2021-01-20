06983499b27b0b8a15ccc1731b39f68aa1a54292
Yevhwnii <mirandoos3223@gmail.com>
2021-01-20 21:46:34 +0100

    first section data

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Premium Bank',
+  headline: 'Unlimited Transactions with zero fees',
+  description:
+    'Get access to our exlusive app that allows you to send unlimited transaction without gerring charged by any fees.',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg.svg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false,
+};
