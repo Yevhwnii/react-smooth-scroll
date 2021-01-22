3d1caaed45d632c5ead47c619e429819623450ab
Yevhwnii <mirandoos3223@gmail.com>
2021-01-22 17:55:57 +0100

    more data objects for info sections

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -15,3 +15,39 @@ export const homeObjOne = {
   primary: true,
   darkText: false,
 };
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Unlimited Access',
+  headline: 'Login to your account at any time',
+  description:
+    'We have you covered no matter where you are located. All you need is an internet connection and a phone or a computer',
+  buttonLabel: 'Learn more',
+  imgStart: true,
+  img: require('../../images/svg-2.svg'),
+  alt: 'Plants',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Join our Team',
+  headline: 'Creating an accound is extremenly easy',
+  description:
+    'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you`re ready to go.',
+  buttonLabel: 'Start now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg'),
+  alt: 'Card',
+  dark: false,
+  primary: false,
+  darkText: true,
+};
