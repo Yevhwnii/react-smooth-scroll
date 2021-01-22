4e819b5c9de6ff39ef0a4d813dcede2b04520ebc
Yevhwnii <mirandoos3223@gmail.com>
2021-01-22 17:56:05 +0100

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages';
+import SigninPage from './pages/Signin';
 
 function App() {
   return (
     <>
-      <Home />
+      <Switch>
+        <Route path='/signin' component={SigninPage} exact />
+        <Route path='/' component={Home} exact />
+      </Switch>
     </>
   );
 }
