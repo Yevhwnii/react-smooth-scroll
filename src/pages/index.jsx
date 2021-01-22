b56b118b0b06909d385a9975168b59bddfaaba38
Yevhwnii <mirandoos3223@gmail.com>
2021-01-22 17:55:32 +0100

    enabling infosections, footer and services

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,13 @@ import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
-import { homeObjOne } from '../components/InfoSection/Data';
+import {
+  homeObjOne,
+  homeObjThree,
+  homeObjTwo,
+} from '../components/InfoSection/Data';
+import Services from '../components/Services';
+import Footer from '../components/Footer';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,6 +25,10 @@ const Home = () => {
       <Navbar toggle={toogleHandler} />
       <HeroSection />
       <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
+      <Footer />
     </>
   );
 };
